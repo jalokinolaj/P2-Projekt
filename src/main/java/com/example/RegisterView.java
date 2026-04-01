e9428cec0adf3c6bce3a45d7074840bec066e986
Chr1sser <csonde25@student.aau.dk>
2026-04-01 11:44:38 +0200

    Diet valgmulighed tilføjet
    valg af diet under registrering af profil

--- src/main/java/com/example/RegisterView.java
+++ src/main/java/com/example/RegisterView.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.UI;
@@ -23,22 +24,27 @@ public class RegisterView extends Composite{
 		TextField username = new TextField("Username");
 		PasswordField password1 = new PasswordField("Password");
 		PasswordField password2 = new PasswordField("Confirm Password");
+		Select<String> diet = new Select<>();
+		diet.setLabel("Diet");	
+		diet.setItems("none", "Vegan", "Vegetarian", "Pescatarian", "Omnivore");	
+		diet.setValue("none");
 		return new VerticalLayout(
 				new H2("Register"),
 				username,
 				password1,
 				password2,
+				diet,
 				new Button("Send", event -> register (
 				username.getValue(),
 				password1.getValue(),
-				password2.getValue()
-						
+				password2.getValue(),	
+				diet.getValue()
 				))
 								
 		);
 	}
 	
-	private void register(String username, String password1, String password2) {
+	private void register(String username, String password1, String password2, String diet) {
 		
 		
 		if (username.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
@@ -57,11 +63,10 @@ public class RegisterView extends Composite{
             
     
     }
-        User user = new User(username, password1);
+        User user = new User(username, password1, diet);
         userRepository.save(user);
         
         Notification.show("Registration successful");
-   
         UI.getCurrent().navigate("");
 	}
 }
